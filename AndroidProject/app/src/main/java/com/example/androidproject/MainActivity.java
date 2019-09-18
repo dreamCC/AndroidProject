7713a947b68a909f8cf56b81a0653d84e5f19946
zhuchaojun <568644031@qq.com>
2019-09-18 18:02:39 +0800

    提交--------

--- AndroidProject/app/src/main/java/com/example/androidproject/MainActivity.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.androidproject;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.hardware.input.InputManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private Button button;
+    private EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +36,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        button = findViewById(R.id.btn_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Button", "an niu dian ji");
+
+
+                if (editText.isFocused()) {
+                    editText.clearFocus();
+                    // 关闭键盘
+                    InputMethodManager mng = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    mng.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
+                }else {
+                    editText.requestFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(editText, InputMethodManager.SHOW_FORCED);// 显示输入法
+                }
+
+            }
+        });
+
+        editText = findViewById(R.id.editText1);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.i("EditText", "beforeTextChanged");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.i("EditText", s.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.i("EditText", "afterTextChanged");
+
+            }
+        });
+
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                Log.i("EditText", String.format("%d", hasFocus?1:0));
+
+            }
+        });
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
