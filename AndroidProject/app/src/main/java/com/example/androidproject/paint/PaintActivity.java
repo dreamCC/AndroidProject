c84abb0215713c9ac4a663ab3984c2a282a5aa07
dreamCC <zhu568644031>
2021-01-19 22:52:35 +0800

    1.19

--- AndroidProject/app/src/main/java/com/example/androidproject/paint/PaintActivity.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/paint/PaintActivity.java
@@ -1,16 +1,19 @@
 package com.example.androidproject.paint;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.androidproject.R;
 
 public class PaintActivity extends AppCompatActivity {
 
-
+    private String TAG = "PaintActivity";
     private ConstraintLayout contentView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +22,10 @@ public class PaintActivity extends AppCompatActivity {
         contentView = (ConstraintLayout) View.inflate(this, R.layout.activity_paint, null);
         setContentView(contentView);
 
+        View rootV =  getWindow().getDecorView().getRootView();
+        Log.i(TAG, "" + rootV);
         didInitializeSubViews();
+
     }
 
     private void didInitializeSubViews() {
